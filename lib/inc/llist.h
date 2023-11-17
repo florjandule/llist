57265b5dd199997f3f94f1c6d64110cec877cdd0
Florjan Dule <florjan.dule.eee@gmail.com>
2023-11-17 22:05:40 +0100

    feat(setup): add basic structure for lib and samples

---
+++ lib/inc/llist.h
@@ -0,0 +1,44 @@
+/**
+ * @file llist.h
+ * @author Florjan Dule (florjan.dule.eee@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-17
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#ifndef _LLIST_H_
+#define _LLIST_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/************************************
+ * INCLUDES
+ ************************************/
+
+/************************************
+ * MACROS AND DEFINES
+ ************************************/
+
+/************************************
+ * TYPEDEFS
+ ************************************/
+
+/************************************
+ * EXPORTED VARIABLES
+ ************************************/
+
+/************************************
+ * GLOBAL FUNCTION PROTOTYPES
+ ************************************/
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _LLIST_H_
---
+++ samples/basic/main.c
@@ -0,0 +1,19 @@
+/**
+ * @file main.c
+ * @author Florjan Dule (florjan.dule.eee@gmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-11-17
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	(void)argc;
+	(void)argv;
+	printf("Hello World!\n");
+}
