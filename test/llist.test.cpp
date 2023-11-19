c6f67a1c57fbe7c7ef93c0d9135aa0313b651530
Florjan Dule <florjan.dule.eee@gmail.com>
2023-11-19 22:02:50 +0100

    test(unit_tests): complemte memory_management_functions test

--- test/llist.test.cpp
+++ test/llist.test.cpp
@@ -2,10 +2,28 @@
 
 #include "llist.c"
 
-TEST(LlistText, llist_set_memory_management_functions)
+void* test_malloc(size_t size)
+{
+	return malloc(size);
+}
+
+void test_free(void* ptr)
+{
+	free(ptr);
+}
+
+TEST(LlistText, llist_set_memory_management_functions_nullptr)
 {
 	EXPECT_NE(llist_set_memory_management_functions(nullptr, nullptr), 0);
-	EXPECT_NE(llist_set_memory_management_functions(malloc, nullptr), 0);
-	EXPECT_NE(llist_set_memory_management_functions(nullptr, free), 0);
-	EXPECT_EQ(llist_set_memory_management_functions(malloc, free), 0);
+	EXPECT_NE(llist_set_memory_management_functions(test_malloc, nullptr), 0);
+	EXPECT_NE(heap_malloc, test_malloc);
+	EXPECT_NE(llist_set_memory_management_functions(nullptr, test_free), 0);
+	EXPECT_NE(heap_free, test_free);
+}
+
+TEST(LlistText, llist_set_memory_management_functions_valid)
+{
+	EXPECT_EQ(llist_set_memory_management_functions(test_malloc, test_free), 0);
+	EXPECT_EQ(heap_malloc, test_malloc);
+	EXPECT_EQ(heap_free, test_free);
 }
