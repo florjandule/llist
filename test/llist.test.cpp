68c7867ceca5198f3026da1d2b421e122e16e126
Florjan Dule <florjan.dule.eee@gmail.com>
2023-12-03 15:06:51 +0100

    feat(unit_tests): add tests fixture

--- test/llist.test.cpp
+++ test/llist.test.cpp
@@ -2,6 +2,14 @@
 
 #include "llist.c"
 
+class LlistTest : public ::testing::Test
+{
+protected:
+	void SetUp() override {}
+
+	void TearDown() override {}
+};
+
 void* test_malloc(size_t size)
 {
 	return malloc(size);
@@ -12,7 +20,7 @@ void test_free(void* ptr)
 	free(ptr);
 }
 
-TEST(LlistText, llist_set_memory_management_functions_nullptr)
+TEST_F(LlistTest, llist_set_memory_management_functions_nullptr)
 {
 	EXPECT_NE(llist_set_memory_management_functions(nullptr, nullptr), 0);
 	EXPECT_NE(llist_set_memory_management_functions(test_malloc, nullptr), 0);
@@ -21,7 +29,7 @@ TEST(LlistText, llist_set_memory_management_functions_nullptr)
 	EXPECT_NE(heap_free, test_free);
 }
 
-TEST(LlistText, llist_set_memory_management_functions_valid)
+TEST_F(LlistTest, llist_set_memory_management_functions_valid)
 {
 	EXPECT_EQ(llist_set_memory_management_functions(test_malloc, test_free), 0);
 	EXPECT_EQ(heap_malloc, test_malloc);
