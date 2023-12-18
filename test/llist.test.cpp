7e98b3ae3fa096ad67c1a3d3c64217edb333e130
Florjan Dule <florjan.dule.eee@gmail.com>
2023-12-18 21:04:22 +0100

    feat(unit_tests): seperate fixture test

--- test/llist.test.cpp
+++ test/llist.test.cpp
@@ -2,25 +2,30 @@
 
 #include "llist.c"
 
-class LlistTest : public ::testing::Test
-{
-protected:
-	void SetUp() override {}
-
-	void TearDown() override {}
-};
-
-void* test_malloc(size_t size)
+static void* test_malloc(size_t size)
 {
 	return malloc(size);
 }
 
-void test_free(void* ptr)
+static void test_free(void* ptr)
 {
 	free(ptr);
 }
+class LlistTestF : public ::testing::Test
+{
+protected:
+	void SetUp() override
+	{
+		llist_set_memory_management_functions(test_malloc, test_free);
+	}
 
-TEST_F(LlistTest, llist_set_memory_management_functions_nullptr)
+	void TearDown() override
+	{
+		/**/
+	}
+};
+
+TEST(LlistTest, llist_set_memory_management_functions_nullptr)
 {
 	EXPECT_NE(llist_set_memory_management_functions(nullptr, nullptr), 0);
 	EXPECT_NE(llist_set_memory_management_functions(test_malloc, nullptr), 0);
@@ -29,7 +34,7 @@ TEST_F(LlistTest, llist_set_memory_management_functions_nullptr)
 	EXPECT_NE(heap_free, test_free);
 }
 
-TEST_F(LlistTest, llist_set_memory_management_functions_valid)
+TEST(LlistTest, llist_set_memory_management_functions_valid)
 {
 	EXPECT_EQ(llist_set_memory_management_functions(test_malloc, test_free), 0);
 	EXPECT_EQ(heap_malloc, test_malloc);
